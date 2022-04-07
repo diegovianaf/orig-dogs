5eefdda8d5f95f829943bdb46cbae1b3268dbe4a
Diego Viana <diegovianaf7@gmail.com>
2022-04-07 18:26:30 -0300

    fi indentation

--- src/Components/Login/LoginCreate.jsx
+++ src/Components/Login/LoginCreate.jsx
@@ -30,15 +30,15 @@ const LoginCreate = () => {
     <section className='animaLeft'>
       <h1 className='title'>Cadastre-se</h1>
       <form onSubmit={handleSubmit}>
-      <Input label="Usuário" type="text" name="username" {...username} />
-      <Input label="Email" type="email" name="email" {...email} />
-      <Input label="Senha" type="password" name="password" {...password} />
-      {loading ? (
-        <Button disabled>Cadastrando...</Button>
-      ) : (
-        <Button>Cadastrar</Button>
-      )}
-      <Error error={error}/>
+        <Input label="Usuário" type="text" name="username" {...username} />
+        <Input label="Email" type="email" name="email" {...email} />
+        <Input label="Senha" type="password" name="password" {...password} />
+        {loading ? (
+          <Button disabled>Cadastrando...</Button>
+        ) : (
+          <Button>Cadastrar</Button>
+        )}
+        <Error error={error}/>
       </form>
     </section>
   )
